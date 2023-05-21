05d31ee0127ca26db3206ff696770a6dae53c081
= <george.bentes@gmail.com>
2023-05-21 14:23:41 -0300

    Allow CORS to every origin

--- src/main/java/com/grglucastr/universeapi/v1/configuration/CORSConfiguration.java
+++ src/main/java/com/grglucastr/universeapi/v1/configuration/CORSConfiguration.java
@@ -14,8 +14,8 @@ public class CORSConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("/**")
-                        .allowedMethods("POST", "GET", "UPDATE", "DELETE", "PUT", "PATCH");
+                        .allowedOrigins("*")
+                        .allowedMethods("POST", "GET", "UPDATE", "DELETE", "PUT", "PATCH", "OPTIONS");
             }
         };
     }
